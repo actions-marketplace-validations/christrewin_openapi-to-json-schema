88ceb48ceedab4e4d60c6acd59470d73eba83bd4
Chris Trewin <chris.trewin@gmail.com>
2021-05-27 14:29:38 +1000

    updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,18 @@ SPEC_URL="${1}"
 
 # retrieve spec file
 CODE=$(curl -sSL -w '%{http_code}' -o openapi.yml $SPEC_URL)
-if [[ "$CODE" =~ ^2 ]]; then
-  # Server returned 2xx response
-  # Attempt to generate a json schema from the openapi spec
-  docker --version
-  file openapi.yml
-# elif [[ "$CODE" = 404 ]]; then
-#     # Server returned 404, so compiling from source
-#     compile_from_source
-else
-  echo "ERROR: server returned HTTP code $CODE"
-  exit 1
-fi
+# if [[ "$CODE" =~ ^2 ]]; then
+#   # Server returned 2xx response
+#   # Attempt to generate a json schema from the openapi spec
+#   docker --version
+#   file openapi.yml
+# # elif [[ "$CODE" = 404 ]]; then
+# #     # Server returned 404, so compiling from source
+# #     compile_from_source
+# else
+#   echo "ERROR: server returned HTTP code $CODE"
+#   exit 1
+# fi
 
 time=$(date)
 echo "::set-output name=time::$time"
